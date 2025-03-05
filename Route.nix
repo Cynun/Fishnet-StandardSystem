d440c657b387d0d4c7ad6dbe54482a6ec0d82038
Cynun Whits <nixos@nixos.>
2025-03-05 05:28:36 +0000

    fix wg conf

--- Route.nix
+++ Route.nix
@@ -17,13 +17,13 @@
 
     wireguard.interfaces.wg0 = {
       listenPort = 51820;
-      postUp = ''
+      postSetup = ''
         ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
         ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.1/24 -o tun0 -j MASQUERADE
         ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
         ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s 2001:db8::1/64 -o tun0 -j MASQUERADE
       '';
-      preDown = ''
+      preShutdown = ''
         ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
         ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o tun0 -j MASQUERADE
         ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
